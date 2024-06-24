ac20cd1782f9937b7dd2a0b1013e6d7534f6cd17
Josh Urdnb <joshua.urdanabia@gmail.com>
2024-06-24 11:58:57 +0200

    Function to create recipe card in JS

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,72 @@
 import { recipes } from "./data/recipes.js"
-console.log(recipes)
+
+// Fonction pour afficher les cartes
+function createRecipeCard(recipe) {
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const prepTime = document.createElement('span')
+    prepTime.className = 'prep-time'
+    prepTime.textContent = `${recipe.time}mn`
+    card.appendChild(prepTime)
+
+    const img = document.createElement('img')
+    img.src = `public/img/${recipe.image}`
+    img.alt = `Image de ${recipe.name}`
+    img.className = 'card-img'
+    card.appendChild(img)
+
+    const cardContent = document.createElement('div')
+    cardContent.className = 'card-content'
+
+    const title = document.createElement('h2')
+    title.textContent = recipe.name
+    cardContent.appendChild(title)
+
+    const recipeHeader = document.createElement('h3')
+    recipeHeader.textContent = 'RECETTE'
+    cardContent.appendChild(recipeHeader)
+
+    const description = document.createElement('p')
+    description.textContent = recipe.description
+    cardContent.appendChild(description)
+
+    const ingredientsHeader = document.createElement('h3')
+    ingredientsHeader.textContent = 'INGRÉDIENTS'
+    cardContent.appendChild(ingredientsHeader)
+
+    const ingredientsWrapper = document.createElement('div')
+    ingredientsWrapper.className = 'ingredients-wrapper'
+
+    recipe.ingredients.forEach((ingredient) => {
+        const ingredientItem = document.createElement('div')
+        ingredientItem.className = 'ingredient-item'
+
+        const ingredientName = document.createElement('p')
+        ingredientName.textContent = ingredient.ingredient
+        ingredientItem.appendChild(ingredientName)
+
+        const ingredientQuantity = document.createElement('p')
+        ingredientQuantity.textContent = `${ingredient.quantity || ''} ${ingredient.unit || ''}`
+        ingredientItem.appendChild(ingredientQuantity)
+
+        ingredientsWrapper.appendChild(ingredientItem)
+    })
+
+    cardContent.appendChild(ingredientsWrapper)
+    card.appendChild(cardContent)
+
+    return card
+}
+
+function displayRecipes(recipes) {
+    const gridWrapper = document.querySelector('.grid-wrapper')
+    gridWrapper.innerHTML = ''
+
+    recipes.forEach((recipe) => {
+        const recipeCard = createRecipeCard(recipe)
+        gridWrapper.appendChild(recipeCard)
+    })
+}
+
+displayRecipes(recipes)
